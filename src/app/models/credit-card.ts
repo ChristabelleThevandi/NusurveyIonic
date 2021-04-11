1aa2fc1eeb7970d1a094395879563ac1c4f9256f
michelavieri <michelavieri@u.nus.edu>
2021-04-11 23:39:53 +0800

    Entity transaction, creditcard, tags

--- src/app/models/credit-card.ts
+++ src/app/models/credit-card.ts
@@ -1,2 +1,23 @@
+import { Transaction } from "./transaction";
+import { User } from "./user";
+
 export class CreditCard {
+    creditCardId: number | undefined;
+    name: string | undefined;
+    card_number: string | undefined;
+    cvv: string | undefined;
+    expiry_date: Date | undefined;
+    balance: number | undefined;
+    
+    user: User | undefined;
+    transactions: Transaction[] | undefined;
+    
+    constructor(name?: string, card_number?: string, cvv?: string, expiry_date?: Date){
+        this.name = name;
+        this.card_number = card_number;
+        this.cvv = cvv;
+        this.expiry_date = expiry_date;
+        this.balance = 0.00;
+        this.transactions = [];
+    }
 }
